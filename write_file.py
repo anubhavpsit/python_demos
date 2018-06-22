0fb7eb77250e286872c0c4e9408aaf920b66a46a
anubhav <anubhav@octane.in>
2018-06-22 17:28:18 +0530

    write text in temp dir

--- write_file.py
+++ write_file.py
@@ -1,6 +1,6 @@
 def main():
     #https://www.guru99.com/reading-and-writing-files-in-python.html
-    f= open("myfile_big.txt","w+")
+    f= open("temp/myfile_big_2.txt","w+")
     for i in range(5000010):
          mobile_number = "958"+str(i) 
          f.write("%s\n" % mobile_number)
